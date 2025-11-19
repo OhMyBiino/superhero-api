418c952ecaf2d710780e765c86b2b19cac0dd3a0
ohmybiino <davenarzobal@gmail.com>
2025-11-19 19:50:11 +0800

    Inject AppDbContext Directly to SuperHero Controller

--- SuperHero.API/Controllers/SuperHeroController.cs
+++ SuperHero.API/Controllers/SuperHeroController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHero.API.DBContext;
 using SuperHero.API.Models;
 
 namespace SuperHero.API.Controllers
@@ -7,30 +9,38 @@ namespace SuperHero.API.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public SuperHeroController(AppDbContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<SuperHeroModel>>> getSuperHeroes()
         {
 
-            var superHeroes = new List<SuperHeroModel>
-            {
-                 new SuperHeroModel
-                {
-                    Id = 1,
-                    Name = "Peter Parker",
-                    FirstName = "Peter",
-                    LastName = "Parker",
-                    Location = "New York"
-                },
-                new SuperHeroModel 
-                {
-                    Id = 2,
-                    Name = "BatMan",
-                    FirstName = "Bruce",
-                    LastName = "Willis",
-                    Location = "Washington"
-                }
-            };
+            //var superHeroes = new List<SuperHeroModel>
+            //{
+            //     new SuperHeroModel
+            //    {
+            //        Id = 1,
+            //        Name = "Peter Parker",
+            //        FirstName = "Peter",
+            //        LastName = "Parker",
+            //        Location = "New York"
+            //    },
+            //    new SuperHeroModel 
+            //    {
+            //        Id = 2,
+            //        Name = "BatMan",
+            //        FirstName = "Bruce",
+            //        LastName = "Willis",
+            //        Location = "Washington"
+            //    }
+            //};
+
+            var superHeroes = await _context.SuperHeroModels.AsNoTracking().ToListAsync();
 
 
             return Ok(superHeroes);
