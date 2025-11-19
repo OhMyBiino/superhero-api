db2466c18a6a6e602fadce58e3df918a3aac361c
ohmybiino <davenarzobal@gmail.com>
2025-11-19 18:55:27 +0800

    Migrate Initial AppDbContext Scheme

---
+++ SuperHero.API/Migrations/20251119104339_Initial Db.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SuperHero.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperHeroModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeroModels", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "SuperHeroModels",
+                columns: new[] { "Id", "FirstName", "LastName", "Location", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Peter", "Parker", "New York City", "Peter Parker" },
+                    { 2, "Tony", "Stark", "Malibu", "Tony Stark" },
+                    { 3, "Steve", "Rogers", "Brooklyn", "Captain America" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHeroModels");
+        }
+    }
+}
