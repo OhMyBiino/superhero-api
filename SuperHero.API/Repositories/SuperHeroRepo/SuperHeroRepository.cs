aad2f85e221d29c1976d350752a1bd5222dedc15
ohmybiino <davenarzobal@gmail.com>
2025-11-24 22:09:53 +0800

    Add Comment for some of Repository Functions

--- SuperHero.API/Repositories/SuperHeroRepo/SuperHeroRepository.cs
+++ SuperHero.API/Repositories/SuperHeroRepo/SuperHeroRepository.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SuperHero.API.DBContext;
 using SuperHero.API.Models;
 
